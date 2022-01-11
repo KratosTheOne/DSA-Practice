82d335c684ce325f27b518023ba739a3f45d6c75
Kaustubh <kaustubhsinha2001@gmail.com>
2022-01-11 13:51:00 +0530

    commit

---
+++ Patterns/rightTriangleMirrored.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = n - 1 - i; j > 0; j--)
+        {
+            cout << ' ';
+        }
+        for(int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
