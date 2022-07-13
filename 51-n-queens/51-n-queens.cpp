07009bc9f8c9901003820aeb803be185ac5de32f
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-07-13 21:29:49 +0530

    Time: 6 ms (82.30%), Space: 7.3 MB (70.91%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
     
+    void queenSolver(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftRow, 
+                     vector<int> &lowerDiag, vector<int> &upperDiag, int n)
+    {
+        if(col == n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row = 0; row < n; row++)
+        {
+            if(leftRow[row] == 0 && lowerDiag[row + col] == 0 && upperDiag[n - 1 + col - row] == 0)
+            {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiag[row + col] = 1;
+                upperDiag[n - 1 + col - row] = 1;
+                queenSolver(col + 1, board, ans, leftRow, lowerDiag, upperDiag, n);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiag[row + col] = 0;
+                upperDiag[n - 1 + col - row] = 0;
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        
+        for(int i = 0; i < n; i++)
+            board[i] = s;
+        
+        vector<int> leftRow(n, 0), lowerDiag(2 * n - 1, 0), upperDiag(2 * n - 1, 0);
+        
+        queenSolver(0, board, ans, leftRow, lowerDiag, upperDiag, n);
+        
+        return ans;
+    }
+    
+    /*
     bool isQueenSafe(int row, int col, vector<string> board, int n)
     {
         int tempRow = row;
@@ -70,4 +113,5 @@ public:
         
         return ans;
     }
+    */
 };
