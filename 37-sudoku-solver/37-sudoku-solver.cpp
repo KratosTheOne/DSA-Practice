d72dd5b24bc77348676292135578ca4fe64e5cfc
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-07-14 20:43:55 +0530

    Time: 62 ms (25.00%), Space: 6.3 MB (79.95%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,3 +1,9 @@
+/*
+Time Complexity: O(9(n ^ 2)), in the worst case, for each cell in the n2 board, we have 9 possible numbers.
+
+Space Complexity: O(1), since we are refilling the given board itself, there is no extra space required, so constant space complexity.
+*/
+
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
