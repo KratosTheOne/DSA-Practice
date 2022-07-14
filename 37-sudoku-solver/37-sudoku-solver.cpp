eaed8c47f82da65c8d28f69b76ed6dfb7f571810
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-07-14 20:42:20 +0530

    Time: 41 ms (59.71%), Space: 6.2 MB (99.93%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+    
+        sudokuSolver(board);
+        
+    }
+    
+    bool sudokuSolver(vector<vector<char>> &board)
+    {
+        for(int i = 0; i < board.size(); i++)
+        {
+            for(int j = 0; j < board[0].size(); j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    for(char c = '1'; c <= '9'; c++)
+                    {
+                        if(isPlaceable(board, i, j, c))
+                        {
+                            board[i][j] = c;
+                            if(sudokuSolver(board) == true)
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isPlaceable(vector<vector<char>> &board, int row, int col, char c)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            if(board[i][col] == c)
+                return false;
+            if(board[row][i] == c)
+                return false;
+            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+};
