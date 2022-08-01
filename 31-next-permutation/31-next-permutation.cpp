740ff67db775615a0cbf5958b21b386a2d4da306
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-01 21:11:20 +0530

    Time: 8 ms (45.68%), Space: 12.2 MB (36.50%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,40 +1,24 @@
+// T.C & S.C -> O(n^2) & O(1)
+
 class Solution {
 public:
-    void swap(vector<int>& nums, int i, int j)
-    {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    void reverse(vector<int>& nums, int start)
-    {
-        int  i = start, j = nums.size() - 1;
-        while(i < j)
-        {
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
-    }
-    
     void nextPermutation(vector<int>& nums) {
-        
-        int i = nums.size() - 2;
-        
-        while(i >= 0 && nums[i + 1] <= nums[i])
-        {
-            i--;
-        }
-        if(i >= 0)
-        {
-            int j = nums.size() - 1;
-            while(nums[j] <= nums[i])
-            {
-                j--;
+        int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
             }
-            swap(nums, i, j);
         }
-        reverse(nums, i + 1);
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
     }
 };
