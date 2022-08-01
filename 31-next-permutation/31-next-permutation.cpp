870332817283d4c8a37657e1f5fd9fe97d20c0f9
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-01 21:24:17 +0530

    Time: 5 ms (65.14%), Space: 12.2 MB (36.50%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,24 +1,37 @@
-// T.C & S.C -> O(n^2) & O(1)
+// T.C & S.C -> O(3 * n) & O(1)
 
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+
         int n = nums.size(), k, l;
-    	for (k = n - 2; k >= 0; k--) {
-            if (nums[k] < nums[k + 1]) {
+
+        //iterate from the back and find the first index lesser than its right element i.e index1
+        for(k = n - 2; k >= 0; k--)
+        {
+            if(nums[k] < nums[k + 1])
+            {
                 break;
             }
         }
-    	if (k < 0) {
-    	    reverse(nums.begin(), nums.end());
-    	} else {
-    	    for (l = n - 1; l > k; l--) {
-                if (nums[l] > nums[k]) {
+
+        //edge case if the array is strictly increasing
+        if(k < 0)
+        {
+            reverse(nums.begin(), nums.end());
+        }
+        else
+        {
+            // iterate from the back and find the first index where value is greater than the value of index1;
+            for(l = n - 1; l > k; l--)
+            {
+                if(nums[l] > nums[k])
+                {
                     break;
                 }
-            } 
-    	    swap(nums[k], nums[l]);
-    	    reverse(nums.begin() + k + 1, nums.end());
+            }
+            swap(nums[k], nums[l]);
+            reverse(nums.begin() + k + 1, nums.end());
         }
     }
 };
