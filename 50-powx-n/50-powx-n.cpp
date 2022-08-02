5536e26576ca03dd0c57bbea18056c9f1074f44f
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-03 00:48:11 +0530

    Time: 2 ms (53.88%), Space: 5.9 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,7 +3,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+       
+        double ans = 1.0;
+        long long positive = abs(n);
         
+        while(positive)
+        {
+            // check for 1 bit
+            if(positive & 1)
+                ans *= x;
+            
+            positive >>= 1;
+            x *= x;
+        }
+        return (n > 0) ? ans : 1 / ans;
+    }
+};
+        /*
         double ans = 1.0;
         long long nn = n;
         
@@ -25,4 +41,4 @@ public:
         if(n < 0) ans = (double)(1.0) / (double)(ans);
         return ans;
     }
-};
+    */
