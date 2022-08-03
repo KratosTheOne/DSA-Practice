a2d073f1f1ca0084e03823ab2a5589f64146e827
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-03 18:54:18 +0530

    Time: 2408 ms (5.00%), Space: 51.1 MB (25.91%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,28 +1,31 @@
+//T.C & S.C -> 3*O(n) & O(1)
+
 class Solution {
 public:
+    
     int longestConsecutive(vector<int>& nums) {
         
-        if(nums.size() == 0){
-            return 0;
-        }
+        set<int> hashSet;
+        for(int num : nums)
+            hashSet.insert(num);
         
-        sort(nums.begin(), nums.end());
+        int longestStreak = 0;
         
-        int n = nums.size();
-        int ans = 1;
-        int prev = nums[0];
-        int curr = 1;
-        
-        for(int i = 1; i < n; i++)
+        for(int num : nums)
         {
-            if(nums[i] == prev+1)
-                curr++;
-            else if(nums[i] != prev)
-                curr = 1;
-            
-            prev = nums[i];
-            ans = max(ans, curr);
+            if(!hashSet.count(num - 1))
+            {
+                int currentNum = num;
+                int currentStreak = 1;
+                
+                while(hashSet.count(currentNum + 1))
+                {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = max(longestStreak, currentStreak);
+            }
         }
-        return ans;
+        return longestStreak;
     }
-};
+};    
