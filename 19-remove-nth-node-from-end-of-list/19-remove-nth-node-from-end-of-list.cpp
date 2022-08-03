50a5762737d5ef21fb83d8c4496c9db4c8a7d4ee
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-04 01:53:51 +0530

    Time: 8 ms (44.48%), Space: 10.7 MB (74.80%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,8 +11,29 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //One Traversal T.C -> O(n)
+        
+        ListNode *start = new ListNode();
+        start -> next = head;
+        
+        ListNode *slow = start;
+        ListNode *fast = start;
+        
+        for(int i = 1; i <= n; i++)
+            fast = fast -> next;
+        
+        while(fast -> next != NULL)
+        {
+            slow = slow -> next;
+            fast = fast -> next;
+        }
+        
+        slow -> next = slow -> next -> next;
+        
+        return start -> next;
         
         
+        /*
         //Two Traversal
         
         if(head == NULL)
@@ -48,7 +69,7 @@ public:
         delete(dummy);
         
         return head;
-        
+        */
         
     }
 };
