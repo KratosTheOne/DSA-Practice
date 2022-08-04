c7e567e6e418221ab12a4ddde6bbfe543a74023e
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-05 01:55:40 +0530

    Time: 12 ms (63.02%), Space: 7.4 MB (99.11%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -13,6 +13,33 @@ public:
         if(head == NULL)
             return 0;
         
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* entry = head;
+        
+        while(fast -> next && fast -> next -> next)
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            
+            if(slow == fast)
+            {
+                while(slow != entry)
+                {
+                    slow = slow -> next;
+                    entry = entry -> next;
+                }
+                return slow;
+            }
+        }
+        return 0;
+         
+        /*
+        T.C & S.C -> O(n) & O(n)
+        
+        if(head == NULL)
+            return 0;
+        
         unordered_set<ListNode*> set;
         ListNode* dummy = head;
         
@@ -26,5 +53,6 @@ public:
         }
         
         return 0;
+        */
     }
 };
