8076729647af12797b2b59b1ab8e50251bacaffb
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-08 00:00:54 +0530

    Time: 25 ms (95.33%), Space: 22.3 MB (55.64%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int xorr = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++)
+            xorr ^= nums[i];
+        
+        return xorr;
+    }
+};
