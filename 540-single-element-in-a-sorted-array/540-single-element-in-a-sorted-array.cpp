63ae0b73f732d16261eafb595140e4f5bd908198
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-08 00:24:24 +0530

    Time: 42 ms (52.21%), Space: 22.4 MB (9.91%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        //T.C & S.C -> O(logn) & O(1)
         
+        int low = 0, high = nums.size() - 2;
+        
+        while(low <= high)
+        {
+            int mid = (low + high) >> 1;
+            
+            if(nums[mid] == nums[mid ^ 1])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        
+        return nums[low];
+        
+        /*
+        //T.C & S.C -> O(n) & O(1)
         int xorr = nums[0];
         
         for(int i = 1; i < nums.size(); i++)
             xorr ^= nums[i];
         
         return xorr;
+        */
     }
 };
