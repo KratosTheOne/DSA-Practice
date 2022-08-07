ec3367d989f98689712b2ba8d4230ff9e07e5084
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-08 01:16:49 +0530

    Time: 8 ms (39.44%), Space: 11.1 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        //T.C & S.C -> O(logn) & O(1)
+        
+        int low = 0, high = nums.size() - 1;
+        while(low <= high)
+        {
+            int mid = (low + high) >> 1;
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            if(nums[low] <= nums[mid])
+            {
+                if(target >= nums[low] && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else
+            {
+                if(target >= nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        
+        return -1;
+            
+        /*
+        T.C & S.C -> O(n) & O(1)
+        
         int n = nums.size(), indi = 0;
         bool flag = false;
         
@@ -15,5 +46,6 @@ public:
         }
         
         return ((flag) ? (indi) : -1);
+        */
     }
 };
