eda7acfb415b86100f28c58e1507c3c26dd6d40d
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-21 23:56:06 +0530

    Time: 48 ms (21.54%), Space: 16.3 MB (71.75%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,35 +1,69 @@
-//T.C & S.C -> O(n) & O(2*n)
+//T.C & S.C -> O(1) & O(n)
 
 class MinStack {
     
-    stack<pair<int, int>> st;
+    stack<long long> st;
+    long long mini;
     
 public:
     MinStack() {
         
+        mini = INT_MAX;
+        
     }
     
-    void push(int val) {
-        int mini;
+    void push(int value) {
         
+        long long val = value;
         if(st.empty())
+        {
             mini = val;
+            st.push(val);
+        }
         else
-            mini = std::min(st.top().second, val);
+        {
+            if(val < mini)
+            {
+                st.push(2 * val * 1LL - mini);
+                mini = val;
+            }
+            else
+                st.push(val);
+        }
         
-        st.push({val, mini});
     }
     
     void pop() {
+        
+        if(st.empty())
+            return;
+        
+        long long el = st.top();
         st.pop();
+        
+        if(el < mini)
+            mini = 2 * mini - el;
+        
     }
     
     int top() {
-        return st.top().first;
+        
+        if(st.empty())
+            return -1;
+        
+        long long el = st.top();
+        
+        if(el < mini)
+            return mini;
+        
+        return el;
+        
     }
     
     int getMin() {
-        return st.top().second;
+        
+        return mini;
+        
     }
 };
 
