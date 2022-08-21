ca0cd3c1e99ce259d4d1424d91c38b39d5fbbeb9
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-21 17:36:14 +0530

    Time: 547 ms (34.81%), Space: 134.8 MB (62.80%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
+    //T.C & S.C -> O(n) & O(n)
+    
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        int n = nums.size();
         
         deque<int> dq;
         vector<int> ans;
         
-        for(int i = 0; i < k; i++)
+        for(int i = 0; i < nums.size(); i++)
         {
-            while(!dq.empty() && nums[i] >= nums[dq.back()])
-                dq.pop_back();
-            dq.push_back(i);
-        }
-        
-        for(int i = k; i < n; i++)
-        {
-            ans.push_back(nums[dq.front()]);
-            
-            while(!dq.empty() && dq.front() <= i - k)
+            if(!dq.empty() && dq.front() == (i - k)) //condition to remove out of bound elements for every k size window
                 dq.pop_front();
             
-            while(!dq.empty() && nums[i] >= nums[dq.back()])
+            while(!dq.empty() && nums[dq.back()] <= nums[i])
                 dq.pop_back();
             
             dq.push_back(i);
+            
+            if(i >= k - 1)
+                ans.push_back(nums[dq.front()]);
         }
-        
-        ans.push_back(nums[dq.front()]);
         
         return ans;
     }
