6af328a2537f629351242aaeb1e85adff31b2138
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-08-26 15:42:08 +0530

    Time: 18 ms (40.47%), Space: 7.5 MB (7.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,7 +1,38 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //T.C & S.C -> O(n) & O(n)
         
+        int n = s.size(), m = t.size();
+        
+        if(n != m)
+            return false;
+        
+        unordered_map<char, int> mp;
+        
+        for(int i = 0; i < n; i++)
+        {
+            mp[s[i]]++;
+        }
+        
+        for(int i = 0; i < m; i++)
+        {
+            if(mp.find(t[i]) != mp.end())
+            {
+                mp[t[i]]--;
+            }
+            if(mp[t[i]] == 0)
+            {
+                mp.erase(t[i]);
+            }
+        }
+        
+        if(mp.size() == 0)
+            return true;
+        
+        return false;
+        
+        /* T.C & S.C -> O(n logn) & O(1)
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         
@@ -20,5 +51,6 @@ public:
         }
         
         return true;
+        */
     }
 };
