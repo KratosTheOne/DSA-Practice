988f88e0352fb79455db363bc132701b1177c76e
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-09-11 03:11:06 +0530

    Time: 3 ms (59.92%), Space: 6.9 MB (98.40%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -1,71 +1,70 @@
-//T.C & S.C -> O(n)
-
 class Twitter {
 public:
+    // to store who each user is following
     unordered_map<int, unordered_set<int>> followers;
-    stack<pair<int, int>> tweets;
+    
+    // to store every tweet made
+    stack<pair<int, int>> global;
     
     Twitter() {
         
     }
     
     void postTweet(int userId, int tweetId) {
+        // add tweet to global stack
+        global.push({userId, tweetId});
         
-        tweets.push({userId, tweetId});
+        // a user (by default) follows themself since their own tweets show in their feed
         followers[userId].insert(userId);
-        
     }
     
     vector<int> getNewsFeed(int userId) {
-        
+        // counter to ensure we only get a max of 10 tweets
         int i = 0;
         
-        vector<int> ans;
+        // return vector
+        vector<int> ret;
         
+        // temporary stack used to restore original global stack after popping tweets
         stack<pair<int, int>> temp;
         
-        while(!tweets.empty() && i < 10)
+        while (!global.empty() && i < 10) 
         {
-            pair<int, int> next = tweets.top();
+            // get next global tweet
+            pair<int, int> next = global.top();
             
-            if(followers[userId].find(next.first) != followers[userId].end())
+            // check if current user is following the user who made this tweet
+            if (followers[userId].find(next.first) != followers[userId].end()) 
             {
-                ans.push_back(next.second);
+                ret.push_back(next.second);
                 i++;
             }
             
-            tweets.pop();
+            global.pop();
             temp.push(next);
         }
-        
-        while(!temp.empty())
+        // restore original global stack of tweets for future use
+        while (!temp.empty()) 
         {
             pair<int, int> next = temp.top();
-            tweets.push(next);
+            global.push(next);
             temp.pop();
         }
         
-        return ans;
+        return ret;
     }
     
-    void follow(int followerId, int followeeId) {
-        
+    void follow(int followerId, int followeeId) 
+    {
+        // insert followee into follower's list
         followers[followerId].insert(followeeId);
         
     }
     
-    void unfollow(int followerId, int followeeId) {
-        
+    void unfollow(int followerId, int followeeId) 
+    {
+        // remove followee from follower's list
         followers[followerId].erase(followeeId);
         
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
