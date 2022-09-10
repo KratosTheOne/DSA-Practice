606e707749b0c4b75c9693c4a4851f77ab80ef9b
Kaustubh Sinha <55058116+KratosTheOne@users.noreply.github.com>
2022-09-10 14:17:27 +0530

    Time: 43 ms (7.98%), Space: 13.6 MB (73.07%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,29 +1,38 @@
+//T.C & S.C -> O(n log n) & O(n)
+
+#define pp pair<int, int>
+
 class Solution {
 public:
-    
-    /*
-    step 1-> store all the values of nums in hashmap to map them .
-    step 2-> create a priority queue and store their corespond value using pair of int, int ;
-    step 3-> itrate the queue till k and push the second value of pair in vector ans and return it ;
-    */
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        map<int, int> mape;
-        for(auto it : nums)
-            mape[it]++;
-        
-        priority_queue<pair<int, int>> pq;
-        for(auto i : mape)
-            pq.push({i.second, i.first});
-        
-        vector<int> ans;
-        while(k--)
+
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++)
         {
-            pair<int, int> p = pq.top();
-            pq.pop();
-            ans.push_back(p.second);
+            mp[nums[i]]++;
         }
-        
+
+        priority_queue<pp, vector<pp>, greater<pp>> minHeap;
+
+        for(auto it = mp.begin(); it != mp.end(); it++)
+        {
+            minHeap.push({it -> second, it -> first});
+
+            if(minHeap.size() > k)
+            {
+                minHeap.pop();
+            }
+        }
+
+        vector<int> ans;
+
+        while(!minHeap.empty())
+        {
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
         return ans;
     }
 };
